ed03df2bc047cc8d15dbff0d93d8db8bbe38b3c9
Xackery <xackery@gmail.com>
2022-01-03 06:37:24 -0800

    Added soldungb

--- soldungb/convert.bat
+++ soldungb/convert.bat
@@ -6,6 +6,9 @@ blender --background %zone%.blend --python C:\src\eqgzi\out\convert.py || goto :
 set last=eqgzi
 eqgzi import %zone% || goto :error
 
+set last=copy
+copy out\* c:\src\demoncia\client\rof\ || goto :error
+
 set last=azone
 cd out 
 azone %zone% || goto :error 
@@ -13,17 +16,17 @@ cd ..
 del out\azone.log
 rmdir /s /q map\
 mkdir map\
-move out\%zone%.map map\
+
+move out\%zone%.map C:\src\server\build\bin\Debug\maps\base\
 
 set last=awater
 cd out 
 awater %zone% || goto :error
 cd ..
 del out\awater.log
-move out\%zone%.wtr map\
 
-set last=copy
-copy out\* c:\src\demoncia\client\rof\ || goto :error
+move out\%zone%.wtr C:\src\server\build\bin\Debug\maps\water\
+
 goto :EOF
 
 :error
